79a799b88f71950f73ec7f5e16540d3b2c7c31d7
koutarou kawase <nkc.koutarou.kawase@gmail.com>
2018-10-23 12:16:30 +0900

    Player貫通床対応、真下にはまだ降りれない

--- Assets/MyFolder/kawase/characterController.cs
+++ Assets/MyFolder/kawase/characterController.cs
@@ -8,37 +8,47 @@ public class characterController : MonoBehaviour {
     public Rigidbody rb;             //body獲得      
     public float jumpPower;          //ジャンプ力
     public float speed;              //移動スピード
-    int key;                          //方向転換に使う
+    int key;                         //方向転換に使う
+    float jumpTime;                  //ジャンプする時間
+    bool jumpOn;                     //ジャンプボタンが押されたか確認
 
     void Start()
     {
     }
 
-
-
     void Jump()
     {
-        if (Input.GetKeyDown("up") && jumpcount < 2)
+        if (Input.GetKeyDown("x") && jumpcount < 2)
         {
             rb.AddForce(new Vector3(0, jumpPower * 45.0f, 0));
+            gameObject.layer = LayerMask.NameToLayer("Jump");
             jumpcount++;
+            jumpOn = true;
+        }
+        if (jumpOn == true)
+        {
+            jumpTime += Time.deltaTime;
+        }
+        if (jumpTime >= 1)
+        {
+            gameObject.layer = LayerMask.NameToLayer("Default");
+            jumpTime = 0f;//タイムの初期化
         }
     }
 
    void CharacterMove()
     {
+        //キャラ移動
         if (Input.GetKey("right"))
         {
             rb.AddForce(Vector3.right * speed, ForceMode.Acceleration);
             key = 1;
-            Debug.Log("右に動く");
         }
 
         if (Input.GetKey("left"))
         {
             rb.AddForce(Vector3.left * speed, ForceMode.Acceleration);
             key = -1;
-            Debug.Log("左に動く");
         }
         //動く方向に応じて反転
         if (key != 0)
@@ -47,10 +57,50 @@ public class characterController : MonoBehaviour {
         }
     }
 
+    void CharacterAttack()
+    {
+        //以下通常攻撃
+        if (Input.GetKeyDown("w"))//□弱攻撃
+        {
+            Debug.Log("(´･ω･`)");
+        }
+
+        if (Input.GetKeyDown("d"))//△強攻撃
+        {
+            Debug.Log("(*´ω｀*)");
+        }
+
+        if (Input.GetKeyDown("a"))//〇スマッシュ
+        {
+            Debug.Log("(#ﾟДﾟ)ｺﾞﾗｧ!!!!");
+        }
+        //以下派生攻撃
+        if (Input.GetKey("q") && Input.GetKeyDown("left"))
+        {
+            Debug.Log("俺の最弱はちっとばっか響くぞ!!!!");
+        }
+
+        if (Input.GetKey("q") && Input.GetKeyDown("right"))
+        {
+            Debug.Log("死んでもいいゲームなんて、ヌル過ぎるぜ");
+        }
+
+        if (Input.GetKey("q") && Input.GetKeyDown("up"))
+        {
+            Debug.Log("青春を楽しむ愚か者ども…砕け散れ");
+        }
+
+        if (Input.GetKey("q") && Input.GetKeyDown("down"))
+        {
+            Debug.Log("やめてよね、本気で喧嘩したらサイが僕にかなうはずないだろ");
+        }
+    }
+
     void Update ()
     {
         CharacterMove();
         Jump();
+        CharacterAttack();
     }
 
     private void OnCollisionEnter(Collision collision)
