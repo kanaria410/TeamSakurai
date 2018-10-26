76270a9e823f9dbf986fce81d0266db2cd0931a2
koutarou kawase <nkc.koutarou.kawase@gmail.com>
2018-10-26 14:07:57 +0900

    移動仕様をいじくりまわす

--- Assets/MyFolder/kawase/YagamiController.cs
+++ Assets/MyFolder/kawase/YagamiController.cs
@@ -1,19 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//川瀬
-public class characterController : MonoBehaviour {
 
-    private int jumpcount;          //ジャンブした回数をカウント
+public class YagamiController : MonoBehaviour
+{
+    //八神唯
     public Rigidbody rb;            //body獲得
     public float jumpPower;         //ジャンプ力
     public float speed;             //移動スピード
+    private int jumpcount;          //ジャンブした回数をカウント
+
     int key;                        //方向転換に使う
-    float jumpTime;                 //ジャンプする時間
-    bool jumpOn;                    //ジャンプボタンが押されたか確認
     int OffensivePower;             //プレイヤーの攻撃力？
     int DefensePower;               //プレイヤーの防御力？
-    bool Awakening;                 //覚醒
+
+    float jumpTime;                 //ジャンプする時間
+    bool jumpOn;                    //ジャンプボタンが押されたか確認
+    bool Awakening;                 //覚醒スイッチ
 
     void Start()
     {
@@ -39,26 +42,35 @@ public class characterController : MonoBehaviour {
         }
     }
 
-   void CharacterMove()
+    void CharacterMove()
     {
         //キャラ移動
         if (Input.GetKey("right"))
         {
             rb.AddForce(Vector3.right * speed, ForceMode.Acceleration);
             key = 1;
-        }
+         }
 
         if (Input.GetKey("left"))
         {
             rb.AddForce(Vector3.left * speed, ForceMode.Acceleration);
             key = -1;
+
         }
+        
         //動く方向に応じて反転
         if (key != 0)
-        {
+         {
             transform.localScale = new Vector3(key, 1, 1);
+         }
+        
+        //ここら辺に回避
+        if (Input.GetKeyDown("e"))
+        {
+            Debug.Log("(ﾟ∀ﾟ)");
         }
     }
+
 
     void CharacterAttack()
     {
@@ -78,24 +90,31 @@ public class characterController : MonoBehaviour {
             Debug.Log("(#ﾟДﾟ)ｺﾞﾗｧ!!!!");
         }
         //以下派生攻撃
-        if (Input.GetKey("q") && Input.GetKeyDown("left"))
+        if (Input.GetKeyDown("q"))
         {
+            Debug.Log("Neutral");
         }
 
-        if (Input.GetKey("q") && Input.GetKeyDown("right"))
+        if (Input.GetKey("q") && Input.GetKeyDown("")
+         || Input.GetKey("q")&& Input.GetKeyDown(""))
         {
+            Debug.Log("左右派生");
         }
+
 
         if (Input.GetKey("q") && Input.GetKeyDown("up"))
         {
+            Debug.Log("上派生");
+
         }
 
         if (Input.GetKey("q") && Input.GetKeyDown("down"))
         {
+            Debug.Log("下派生");
         }
     }
 
-    void Update ()
+    void Update()
     {
         CharacterMove();
         Jump();
@@ -107,8 +126,7 @@ public class characterController : MonoBehaviour {
         if (collision.gameObject.tag == "ground")
         {
             jumpcount = 0;
-            Debug.Log("初期化済み");
+            Debug.Log("初期化済み"); 
         }
     }
 }
-
