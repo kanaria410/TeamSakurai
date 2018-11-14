757abe89e7823444af197630c6ea5d80118364ee
Kaito Itou <nkc.kaito.itou@gmail.com>
2018-11-14 10:12:04 +0900

    パーティクルを何秒後に削除するかというスクリプトを作成

---
+++ Assets/MyFolder/itou/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField, Header("何秒で削除するか")]
+    float destroyTime;
+
+	// Use this for initialization
+	void Start ()
+    {
+        ParticleSystem particleSystem = GetComponent<ParticleSystem>();
+        Destroy(gameObject, destroyTime);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+}
