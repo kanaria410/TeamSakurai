3f73271d6230b374c76d4bf9f06124c3aa7cf9ee
Kaito Itou <nkc.kaito.itou@gmail.com>
2018-12-05 13:47:17 +0900

    Scriptsフォルダーを作成し、シーン移行処理を書いたスクリプトを作成

---
+++ Assets/MyFolder/itou/Scripts/SceneChange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    [SerializeField, Header("飛びたいシーンの名前")]
+    string sceneName;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //仮のイベント
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene(sceneName);
+        }	
+	}
+}
